1ad8f1f9a3771fc107a9d9c20c933f3e94d5be9d
leanml03 <lean.montero.leiton@gmail.com>
2022-10-28 17:06:15 -0600

    -Falta Insercion JSON al Main y conversion a DIct

--- page/page.py
+++ page/page.py
@@ -1,9 +1,12 @@
 #Its necessary to install flask: pip install flask
 #imports
+import json
 from flask import Flask, render_template, redirect, url_for,request
 app = Flask(__name__)
 
 containers=[('Crunchyroll','Anime'),('Youtube','Videos'),('Twitter','Red Social'),('Facebook','Red Social'),('RedTube','Red Social')]
+pages=[{"page":"namepage", "Column01":"data", "Column02":"data"},{"page":"namepage2", "Column01":"data", "Column02":"data"}]
+
 @app.route("/",methods=["POST","GET"])
 def home():
     if request.method=="POST":
@@ -15,8 +18,14 @@ def home():
 @app.route("/<cat>")
 def categories(cat):
     #return f"<h1>{cat}</h1>"
+    pagesList=[] #Paginas ya revisadas conforme a la categoria
+    #Clasificacion de las paginas por categoria
+    #for i in pages:
+        #currentPage=json.load(i) #esto sirve para cargar el JSON
+        #if(currentPage['Category']==cat): Esto sirve para clasificar la pagina conforme la categoria
+            #pagesList.append(currentPage)
     print("La categoria es",cat)
-    return render_template('index.jinja2',category=cat)
+    return render_template('index.jinja2',category=cat,data=pages)
         
     
 if __name__=="__main__":
